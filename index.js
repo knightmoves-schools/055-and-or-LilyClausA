0d72cae39c756b7aafaf859efb1d4e1bf53e7e6a
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-12 20:05:40 +0000

    sxdgbhnju

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+function taxable(state == state && taxExempt == taxExempt){
+    if (state == IA && taxExempt == false){
+        return "is taxable";
+    }
+}
+
+//should create a function named taxable that takes two parameters 
+// state and taxExempt and returns the string "is taxable" if the 
+// state is IA and taxExempt is false
+
+
+
+
+//should not return the string "is taxable" if the state is IA and 
+// taxExempt is true
+
+//should not return the string "is taxable" if the state is FL and 
+// taxExempt is false
+
+//should create a function named isVehicle that takes three parameters 
+// hasWheels, canFly, canSwim and returns the string "is vehicle" if 
+// at least one of the three parameters evaluates to true
+
+//should return the string "is vehicle" when hasWheels is true and 
+// canFly and canSwim are both false
+
+//should return the string "is vehicle" when canFly is true and hasWheels
+//  and canSwim are both false
+
+//should return the string "is vehicle" when canSwim is true and canFly 
+// and hasWheels are both false
+
+//should return the string "is vehicle" when canSwim and canFly are both 
+// true and hasWheels is false
+
+//should not return the string "is vehicle" when hasWheels, canFly, and 
+// canSwim are all false
